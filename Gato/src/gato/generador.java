a206539ad370a98e389549c2d02d1519291d1ba6
juanfe2898 <43549942+juanfe2898@users.noreply.github.com>
2018-09-27 17:34:22 -0500

    Add files via upload

---
+++ Gato/src/gato/generador.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gato;
+
+/**
+ *
+ * @author juanf
+ */
+public class generador {
+   
+
+/*
+   char [][]m ={{' ',' ',' '}, {' ', ' ', ' '}, {' ',' ',' ',}} ;
+        for(int i=0;i<3; i++){
+            m=p.get(i).getM();
+           for(int j=0;j<3; j++){
+               for(int k=0;k<3; k++){
+                   
+               }
+           }
+        }
+   */
+   
+}
---
+++ Gato/src/gato/juego.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gato;
+
+/**
+ *
+ * @author juanf
+ */
+public class juego {
+   int id=1;
+   int idPadre=0;
+   int prob;
+   int minmax;//1 max//0 min
+   char m [][]=new char[][]{{' ',' ',' '}, {' ',' ',' '}, {' ',' ',' '}};;
+
+    public juego( int minmax) {
+        this.id = id;
+        this.idPadre = idPadre;
+        this.prob = prob;
+        this.minmax = minmax;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdPadre() {
+        return idPadre;
+    }
+
+    public void setIdPadre(int idPadre) {
+        this.idPadre = idPadre;
+    }
+
+    public int getProb() {
+        return prob;
+    }
+
+    public void setProb(int prob) {
+        this.prob = prob;
+    }
+
+    public int getMinmax() {
+        return minmax;
+    }
+
+    public void setMinmax(int minmax) {
+        this.minmax = minmax;
+    }
+
+    public char[][] getM() {
+        return m;
+    }
+
+    public void setM(char[][] m) {
+        this.m = m;
+    }
+
+   }
+
+
